2dbfe55ff3b53676c532cbe74f20fbb68307554c
Elizabeth <itsalaz@icloud.com>
2024-06-10 19:12:42 -0400

    lab completed

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(tutorial => { 
+  const arrayOfWords = tutorial.split(" ")
+  return arrayOfWords.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1) }) .join(" ")
+})
+
+  console.log(titleCased)
+
+
